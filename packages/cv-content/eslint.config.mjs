f709b4dca527f5958b15aa72d68761b735d4f486
devtobi <28538704+devtobi@users.noreply.github.com>
2024-06-03 23:09:11 +0200

    ♻️ Refactored to monorepo

--- packages/cv-content/eslint.config.mjs
+++ packages/cv-content/eslint.config.mjs
@@ -6,17 +6,12 @@ export default [
   js.configs.recommended,
   eslintConfigPrettier,
   {
-    files: ["packages/cv-content/**"],
     languageOptions: {
       sourceType: "commonjs",
       globals: {
         ...globals.node,
       },
     },
-    ignores: ["node_modules", "eslint.config.mjs"],
-  },
-  {
-    files: ["package/cv-wrapper/**"],
     ignores: ["node_modules", "eslint.config.mjs"],
   },
 ];
