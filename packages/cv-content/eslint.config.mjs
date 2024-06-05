b1377c8edd7ca0b5cd68a022e59582de41d6bf72
devtobi <28538704+devtobi@users.noreply.github.com>
2024-06-05 22:24:11 +0200

    🎨 Added centralized prettier configuration and formatted files

--- packages/cv-content/eslint.config.mjs
+++ packages/cv-content/eslint.config.mjs
@@ -1,17 +1,17 @@
-import eslintConfigPrettier from "eslint-config-prettier";
-import js from "@eslint/js";
-import globals from "globals";
+import eslintConfigPrettier from 'eslint-config-prettier'
+import js from '@eslint/js'
+import globals from 'globals'
 
 export default [
   js.configs.recommended,
   eslintConfigPrettier,
   {
     languageOptions: {
-      sourceType: "commonjs",
+      sourceType: 'commonjs',
       globals: {
-        ...globals.node,
-      },
+        ...globals.node
+      }
     },
-    ignores: ["node_modules", "eslint.config.mjs"],
-  },
-];
+    ignores: ['node_modules', 'eslint.config.mjs']
+  }
+]
--- packages/cv-content/scripts/generatePdfs.js
+++ packages/cv-content/scripts/generatePdfs.js
@@ -1,8 +1,8 @@
-const execConcurrently = require("./helpers/execConcurrently.js");
-const generateTasks = require("./helpers/generateTasks.js");
-const { DIST_DIRECTORY } = require("./helpers/constants.js");
+const execConcurrently = require('./helpers/execConcurrently.js')
+const generateTasks = require('./helpers/generateTasks.js')
+const { DIST_DIRECTORY } = require('./helpers/constants.js')
 
-const GENERATE_PDF_CMD = `node scripts/helpers/generatePdf.js --input ${DIST_DIRECTORY}/%s/index.html --output ${DIST_DIRECTORY}/%s/cv.pdf`;
+const GENERATE_PDF_CMD = `node scripts/helpers/generatePdf.js --input ${DIST_DIRECTORY}/%s/index.html --output ${DIST_DIRECTORY}/%s/cv.pdf`
 
-const generatePdfTasks = generateTasks("build-pdf", GENERATE_PDF_CMD);
-execConcurrently(generatePdfTasks);
+const generatePdfTasks = generateTasks('build-pdf', GENERATE_PDF_CMD)
+execConcurrently(generatePdfTasks)
