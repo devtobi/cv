01b43e3b8a85de640d7b2fa170a9e11584af2629
devtobi <28538704+devtobi@users.noreply.github.com>
2024-06-09 23:14:15 +0200

    fix(config): fix ESLint config of cv-wrapper

---
+++ packages/cv-wrapper/.eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:@intlify/vue-i18n/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  settings: {
+    'vue-i18n': {
+      localeDir: './src/locales/*.json',
+      messageSyntaxVersion: '9.13.1',
+    },
+  },
+};
