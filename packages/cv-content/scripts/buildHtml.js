cc9e7784be2afeba47c610f483d1f99faeae59ce
devtobi <28538704+devtobi@users.noreply.github.com>
2025-05-02 20:34:29 +0200

    :wrench: Update PDF export command

--- packages/cv-content/scripts/buildHtml.js
+++ packages/cv-content/scripts/buildHtml.js
@@ -3,7 +3,7 @@ const execConcurrently = require('./helpers/execConcurrently.js');
 const generateTasks = require('./helpers/generateTasks.js');
 const { DATA_DIRECTORY, DIST_DIRECTORY } = require('./helpers/constants.js');
 
-const BUILD_CMD = `resumed render ${DATA_DIRECTORY}/%s.json -o ${DIST_DIRECTORY}/%s/index.html -t $THEME`;
+const BUILD_CMD = `resumed export ${DATA_DIRECTORY}/%s.json -o ${DIST_DIRECTORY}/%s/index.html -t $THEME`;
 
 createDirectories();
 const buildTasks = generateTasks('build-html', BUILD_CMD);
