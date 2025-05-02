457f5767ddac5e44678c98c0ad1623314017620b
devtobi <28538704+devtobi@users.noreply.github.com>
2025-05-02 20:59:45 +0200

    :wrench: Simplified build by removing custom pdf scripts

--- packages/cv-content/scripts/buildPdf.js
+++ packages/cv-content/scripts/buildPdf.js
@@ -3,8 +3,8 @@ const execConcurrently = require('./helpers/execConcurrently.js');
 const generateTasks = require('./helpers/generateTasks.js');
 const { DATA_DIRECTORY, DIST_DIRECTORY } = require('./helpers/constants.js');
 
-const BUILD_CMD = `resumed render ${DATA_DIRECTORY}/%s.json -o ${DIST_DIRECTORY}/%s/index.html -t $THEME`;
+const BUILD_CMD = `resumed export ${DATA_DIRECTORY}/%s.json -o ${DIST_DIRECTORY}/%s/cv.pdf -t $THEME`;
 
 createDirectories();
-const buildTasks = generateTasks('build-html', BUILD_CMD);
+const buildTasks = generateTasks('build-pdf', BUILD_CMD);
 execConcurrently(buildTasks);
