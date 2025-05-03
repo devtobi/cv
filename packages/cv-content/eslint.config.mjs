e0412cbf79686ad95578fdf1f298f9f90be2a647
devtobi <28538704+devtobi@users.noreply.github.com>
2025-05-03 14:19:23 +0200

    :wrench: Updated ESLint configuration for cv-content

--- packages/cv-content/eslint.config.mjs
+++ packages/cv-content/eslint.config.mjs
@@ -1,19 +1,25 @@
-import js from '@eslint/js';
+import jsEslintConfig from '@eslint/js';
+import { ESLint } from 'eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 
-export default [
-  js.configs.recommended,
+export default defineConfig([
+  ESLint.defaultConfig,
+  jsEslintConfig.configs.recommended,
   eslintConfigPrettier,
   {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+      reportUnusedInlineConfigs: 'error',
+    },
     languageOptions: {
-      sourceType: 'module',
       globals: {
         ...globals.node,
       },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
   },
-  {
-    ignores: ['node_modules', 'dist'],
-  },
-];
+  globalIgnores(['node_modules', 'dist']),
+]);
